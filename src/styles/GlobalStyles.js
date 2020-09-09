3cf716db9da563f76a0070be448bc2d150632821
Antony Memia <antony@olywave.com>
2020-09-09 23:04:17 +0300

    Simple form

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const MainContainer = styled.div`
+    width: 100%;
+    padding-right: 15px;
+    padding-left: 15px;
+    margin-right: auto;
+    margin-left: auto;
+    max-width:960px;
+`;
+
+export const Row = styled.div`
+    display: -ms-flexbox;
+    display: flex;
+    -ms-flex-wrap: wrap;
+    flex-wrap: wrap;
+    margin-right: -15px;
+    margin-left: -15px;
+`;
+
+
